3086e0d004983e3c02fd1e30178bfd522fcf9665
Michal <mincoslav@gmail.com>
2022-04-06 18:47:11 +0200

    Updated payload definition based on mongo schema

--- payload_definitions/__init__.py
+++ payload_definitions/__init__.py
@@ -1,25 +1,36 @@
-from pydantic import (BaseModel, conlist)
+from pydantic import BaseModel, conlist
 from datetime import datetime
+from enum import Enum
+
+
+# TODO: We might not even need this tbh
+class EventType(Enum):
+    Pregnancy = "Pregnancy"
+    Crime = "Crime"
+    HealthProblem = "Health Problem"
+    Fire = "Fire"
+    Accident = "Accident"
+    Violence = "Violence"
+
+
+class Status(Enum):
+    Received = "received"
+    MessageSent = "message_sent"
+    MessageError = "message_error"
+    Resolved = "resolved"
+
 
 class Location(BaseModel):
-    type: str                                                # We might want to keep radius/point so use this to ensure schema integrity
-    coordinates: conlist(float, min_items=2, max_items=2)    # lattitude, longitude
-    last_updated: datetime                                   # or whatever is compatible with Mongo
+    type: str  # We might want to keep radius/point so use this to ensure schema integrity
+    coordinates: conlist(float, min_items=2, max_items=2)  # lattitude, longitude
+    last_updated: datetime  # or whatever is compatible with Mongo
     country: str
 
-class UserType(BaseModel):
-    victim: bool    
-    responder: bool
 
-
-class User(BaseModel):
-    email: str
-    phone_number: str
-    location: Location
-    user_type: UserType
-
-class LatestLocation(BaseModel):
-    user_uid:str
-    date: datetime
-    user_type: UserType
-    location: Location
+class ButtonPressEvent(BaseModel):
+    event_type: EventType
+    current_location: Location
+    userID: str
+    responderID: str
+    status: Status
+    resolved: bool
