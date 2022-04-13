fa6085f5bf3d87b79eaa2a907b5beddeca275d7e
Michal <mincoslav@gmail.com>
2022-04-13 16:43:52 +0200

    schema/payload updated

--- mongo_access_funcs/__init__.py
+++ mongo_access_funcs/__init__.py
@@ -1,17 +1,22 @@
-from typing import Union
+from typing import List, Union
+from pydantic import conlist
 import pymongo
 import os
 from pymongo.collection import Collection
 import os
 
-from payload_definitions import EventType 
+from payload_definitions import EventType
 
 ATLAS_CONNECTION = os.getenv("ATLAS_CONNECTION_STRING")
-DATABASE_NAME = "bpr-backend"
+BPR_BACKEND = "bpr-backend"
 EVENT_COLLECTION = "events"
 
 
-def get_collection(database_name: str, collection_name: str, connection_string: str=ATLAS_CONNECTION):
+def get_collection(
+    database_name: str = BPR_BACKEND,
+    collection_name: str = EVENT_COLLECTION,
+    connection_string: str = ATLAS_CONNECTION,
+):
     client = pymongo.MongoClient(connection_string)
     database = client[database_name]
     collection = database[collection_name]
@@ -19,26 +24,34 @@ def get_collection(database_name: str, collection_name: str, connection_string: 
 
 
 # TODO: maybe add some validation?
-def post_document(document: Union[dict, EventType], collection: Collection):
-    if collection == EVENT_COLLECTION:
-        document is EventType
+def post_document(
+    document: Union[dict, EventType], collection: Collection = get_collection()
+):
     return collection.insert_one(document)
 
 
-def get_document_by_ID(documentID: str, collection: Collection):
-    return collection.find_one({"_id": "{documentID}".format(documentID=documentID)})
+def get_document_by_ID(documentID: str, collection: Collection = get_collection()):
+    return collection.find_one({"_id": "{documentID}".format(documentID)})
 
 
+def get_documents_within_range(
+    coordinates: conlist(float, min_items=2, max_items=2),
+    radius: float,
+    collection: Collection = get_collection(),
+):
+    collection.find({})
+    return True
 
-# TODO: this would depend on other stuff.
+
 # Need to figure out user/requests data models before implementing this.
+# TODO: this would depend on other stuff.
 # For the time being this is harcoded for bpr-tester -> events
 # def get_event(event_ID: str):
-#     collection = get_collection(DATABASE_NAME, EVENT_COLLECTION)
+#     collection = get_collection(BPR_BACKEND, EVENT_COLLECTION)
 #     collection.find_one({})
 #     return True
 
 
-#TODO: To be implemented
+# TODO: To be implemented
 # def get_closest_responders(location: list):
 #     return True
