fc6e51474ad861d98a2a2afb9c77b367afcb865e
Michal <mincoslav@gmail.com>
2022-05-03 09:48:04 +0200

    removed dependency

--- notifications/__init__.py
+++ notifications/__init__.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import firebase_admin
 
 
 # sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
@@ -8,4 +7,4 @@ import firebase_admin
 # cred = credentials.Certificate("C:/Users/minco/OneDrive/Desktop/vs-code-projects/bpr-backend/firebase_funcs/firebase_service_account.json")
 
 def get_app():
-    return firebase_admin.initialize_app()
+    # return firebase_admin.initialize_app()
