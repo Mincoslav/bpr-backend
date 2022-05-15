89cdfc4599b2b3ff0c3efc9432c1acc79af0b4ea
Michal <mincoslav@gmail.com>
2022-05-15 20:11:02 +0200

    added testing folder

--- test/test_api.py
+++ test/test_api.py
@@ -1,1 +1,14 @@
-from 
+from fastapi.testclient import TestClient
+
+import app
+
+
+client = TestClient(app.app)
+
+
+def test_read_root():
+
+    response = client.get("/")
+    print(response.content)
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "Bachelors"}
